d7bb93fde03fb671fcd025f27be39f8b9c17ff49
DiegoE <diegoesc8712@hotmail.com>
2021-01-15 09:27:29 -0500

    Componentes

---
+++ curso-angular4/src/app/empleado/empleado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'empleado-tag',
+    templateUrl: './empleado.component.html'
+    // styleUrls: ['./fruta.component.css']
+  })
+
+  export class EmpleadoComponent {
+    public Titulo = 'Titulo del Componente de Empleado'
+  }
