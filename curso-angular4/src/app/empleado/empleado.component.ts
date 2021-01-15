a4afce50c9e82d90e92444c8fcd6157c30e02880
DiegoE <diegoesc8712@hotmail.com>
2021-01-15 14:53:08 -0500

    TypeScript

--- curso-angular4/src/app/empleado/empleado.component.ts
+++ curso-angular4/src/app/empleado/empleado.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado';
+
 @Component({
     selector: 'empleado-tag',
     templateUrl: './empleado.component.html'
-    // styleUrls: ['./fruta.component.css']
   })
 
   export class EmpleadoComponent {
-    public Titulo = 'Titulo del Componente de Empleado'
+    public Titulo = 'Componente Empleados:';
+
+    public empleado:Empleado;
+    public trabajadores:Array<Empleado>;
+
+    constructor(){
+      this.empleado = new Empleado('Diego', 33, 'dev', true);
+      this.trabajadores = [
+        new Empleado('Diego', 33, 'dev', true),
+        new Empleado('Alejandro', 23, 'programador', false),
+        new Empleado('Monica', 34, 'contadora', true)
+      ];
+
+    }
+
+    ngOnInit(){
+      console.log(this.empleado);
+      console.log(this.trabajadores);
+    }
   }
--- curso-angular4/src/app/fruta/fruta.component.ts
+++ curso-angular4/src/app/fruta/fruta.component.ts
@@ -8,4 +8,46 @@ import { Component } from '@angular/core';
   export class FrutaComponent {
     public nombre_componente = 'Componente de fruta';
     public listado_frutas = 'Naranja, Manzana, Pera y Sandia'
+
+    public nombre:string;
+    public edad:number;
+    public MayorEdad:boolean;
+    public trabajos:Array<any>=['Carpintero', 'desarrollador', 55]
+    comodin:any;
+
+
+    constructor(){
+      this.nombre = 'Diego';
+      this.edad = 33;
+      this.MayorEdad = true;
+      this.comodin = 'Cualquier cosa';
+    }
+
+    ngOnInit(){
+      this.cambiarNombre();
+      this.cambiarEdad(25);
+
+      console.log(this.nombre +" "+ this.edad);
+
+
+      // variables var y let y su alcance
+      var num1 = 8;
+      var num2 = 15;
+      if (num1 === 8) {
+        let num1 = 3;
+        var num2 = 88;
+
+        console.log("dento del if "+num1+"    "+num2);
+      }
+      console.log("fuera del if "+num1+"    "+num2);
+
+    }
+
+    cambiarNombre(){
+      this.nombre = 'Alejandro';
+    }
+
+    cambiarEdad(edad){
+      this.edad = edad;
+    }
   }
